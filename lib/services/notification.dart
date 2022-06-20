870674b66a9f10e2b82ddb9cb28022252600e3f3
ndth4ng <thangxacam@gmail.com>
2022-06-20 19:27:58 +0700

    feat/notification: real time notification with delete

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -15,10 +15,10 @@ class NotificationService {
     populatedNotification.userSend = userSendInfo;
 
     // populate user receive
-    // DocumentSnapshot userSendSnap = await notification.userSendRef.get();
-    // Author? userSendInfo =
-    //     Author.fromJson(userSendSnap.data() as Map<String, dynamic>);
-    // populatedNotification.userSend = userSendInfo;
+    DocumentSnapshot userReceiveSnap = await notification.userReceiveRef.get();
+    Author? userReceiveInfo =
+        Author.fromJson(userReceiveSnap.data() as Map<String, dynamic>);
+    populatedNotification.userReceive = userReceiveInfo;
 
     //populate post if have
     if (notification.postRef != null) {
@@ -28,5 +28,40 @@ class NotificationService {
     }
 
     return populatedNotification;
+  }
+
+  Stream<List<NotificationType>> getNotifications(String userId) {
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('notifications')
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .asyncMap((snapshot) {
+      return Future.wait(snapshot.docs.map((document) async {
+        NotificationType notification =
+            NotificationType.fromJson(document.data());
+
+        notification = await populateNotification(notification);
+
+        return notification;
+      }).toList());
+    });
+  }
+
+  // .withConverter<NotificationType>(
+  //         fromFirestore: (snapshot, _) =>
+  //             NotificationType.fromFirestore(snapshot, _),
+  //         toFirestore: (NotificationType notification, _) =>
+  //             notification.toFirestore());
+
+  deleteNotification(String notificationId, String userReceiveId) {
+    print('$userReceiveId $notificationId');
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(userReceiveId)
+        .collection('notifications')
+        .doc(notificationId)
+        .delete();
   }
 }
